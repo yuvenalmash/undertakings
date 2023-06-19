effd6d383ffb1a026596abc83c1190d408753257
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-20 02:57:18 +0300

    Create sessions controller to handle jwt generation

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user&.valid_password?(params[:session][:password])
+      token = user.generate_jwt
+      render json: { token: token, email: user.email }, status: :ok
+    else
+      render json: { errors: 'Invalid email or password' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
