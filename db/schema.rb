0a8dc4dab55cfb4a0e9f6eafcab2cb72b46e154f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-19 21:54:15 +0300

    Create task model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_175928) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_182818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "due_date"
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["completed"], name: "index_tasks_on_completed"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 50, null: false
@@ -23,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_175928) do
     t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "tasks", "users"
 end
