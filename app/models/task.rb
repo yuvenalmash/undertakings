5ce97f535d04d223035ff83209827952535800c3
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-19 22:48:31 +0300

    Add model associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+
+  # Associations
   belongs_to :user
+
+  # Scopes
+  scope :completed, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  # Associations
+  has_many :tasks, dependent: :destroy
 end
