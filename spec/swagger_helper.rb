23e6ecdb0b96f5cd3ac2d01fb30043e978ca5c46
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-07-06 11:31:31 +0300

    Update host in swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -25,7 +25,8 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: '127.0.0.1:3000'
+              default: 'undertakings-5fbcc97dce19.herokuapp.com'
+              # default: '127.0.0.1:3000'
             }
           }
         }
