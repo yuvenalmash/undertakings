a527b848c2bc9104f38bc61aa8369fe48147d560
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-07-08 02:24:03 +0300

    Add debugging code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::API
     return render json: { error: 'Token not found' }, status: :unauthorized unless token
 
     decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+    puts 'decoded_token: ', decoded_token
     payload = decoded_token.first
+    puts 'payload: ', payload
     @current_user = User.find_by(id: payload['id'])
+    puts '@current_user: ', @current_user
     render json: { error: 'Invalid token' }, status: :unauthorized unless @current_user
   rescue JWT::ExpiredSignature
     render json: { error: 'Token expired' }, status: :unauthorized
